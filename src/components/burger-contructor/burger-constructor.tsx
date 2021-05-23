bd1d9993541afa1ef31de62ea57f68193b727bf8
Aristov Yuriy <lonelyme.v@gmail.com>
2021-05-23 20:44:49 +0500

    Добавлена иконка DragIcon в конструктор

--- src/components/burger-contructor/burger-constructor.tsx
+++ src/components/burger-contructor/burger-constructor.tsx
@@ -3,10 +3,11 @@ import { Ingredient } from '../../utils/ingredients';
 import {
   Button,
   ConstructorElement,
-  CurrencyIcon
+  CurrencyIcon, DragIcon
 } from '@ya.praktikum/react-developer-burger-ui-components';
 import { useConstructorHooks } from '../../state/providers/constructor-provider';
 import styles from './burger-constructor.module.css'
+import classnames from 'classnames';
 
 interface OwnProps {
 }
@@ -19,36 +20,59 @@ const BurgerConstructor: FunctionComponent<Props> = () => {
   const [price, setPrice] = useState(0);
   useEffect(() => {
     setBun(items?.filter(e => e.type === 'bun')[0]);
-    setPrice(items!.reduce((prev, curr) => prev + curr.price,0));
+    setPrice(items!.reduce((prev, curr) => prev + curr.price, 0));
   }, [items]);
   return (
-    <div className={'mt-25'} style={{ flex: '1', maxWidth: '592px' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-        { bun && <ConstructorElement
-            type='top' isLocked={true}
-            text={bun.name + '(верх)'}
-            thumbnail={bun.image}
-            price={bun.price}
-        />}
-        <div className={styles['burger-constructor-scroller']}>
+    <div className={ 'mt-25' } style={ { flex: '1', maxWidth: '592px' } }>
+      <div style={ { display: 'flex', flexDirection: 'column', gap: '10px' } }>
+        { bun && (
+          <div className={ styles['burger-constructor-item'] }>
+            <ConstructorElement
+              type="top" isLocked={ true }
+              text={ bun.name + '(верх)' }
+              thumbnail={ bun.image }
+              price={ bun.price }
+            />
+          </div>
+        ) }
+        <div className={ styles['burger-constructor-scroller'] }>
           {
             items!.filter(e => e.type !== 'bun').map((e, idx) => {
-              return (<ConstructorElement
-                key={`&{e._id}_${idx}`}
-                text={e.name}
-                thumbnail={e.image}
-                price={e.price}
-                handleClose={() => removeIngredient && removeIngredient(e._id)}
-              />)
+              return (
+                <div
+                  className={
+                    classnames(
+                      styles['burger-constructor-item'],
+                      styles['burger-constructor-item__draggable']
+                    )}
+                >
+                  <DragIcon type={ 'primary' }/>
+                  <ConstructorElement
+                    key={ `&{e._id}_${ idx }` }
+                    text={ e.name }
+                    thumbnail={ e.image }
+                    price={ e.price }
+                    handleClose={ () => removeIngredient && removeIngredient(e._id) }
+                  />
+                </div>
+              )
             })
           }
         </div>
-        { bun && <ConstructorElement type='bottom' isLocked={true} text={bun.name + '(низ)'} thumbnail={bun.image} price={bun.price} />}
+        { bun && (
+          <div className={ styles['burger-constructor-item'] }>
+            <ConstructorElement
+              type="bottom"
+              isLocked={ true }
+              text={ bun.name + '(низ)' }
+              thumbnail={ bun.image } price={ bun.price }/>
+          </div>
+        ) }
       </div>
-      <div className={'mt-10'}>
+      <div className={ 'mt-10' }>
         <span className="text text_type_digits-medium mr-10">
-          {price}
-          <CurrencyIcon type={'primary'} />
+          { price }
+          <CurrencyIcon type={ 'primary' }/>
         </span>
         <Button
           type="primary"
