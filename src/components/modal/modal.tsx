850967dbce609882dc1f8ee5a14c74611dc1740f
Aristov Yuriy <lonelyme.v@gmail.com>
2021-06-04 18:55:14 +0500

    Обработчик onkeyPress повешен на document

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './modal.module.css';
 
@@ -8,14 +8,20 @@ type Props = {
 }
 
 export default function Modal({ children, title, onClose }: React.PropsWithChildren<Props>) {
-  const onKeyPress: React.KeyboardEventHandler = (evt) => {
-    if (evt.key === 'Escape') {
-      onClose();
+  useEffect(() => {
+    function onKeyPress(evt: any) {
+      if (evt.key === 'Escape') {
+        onClose();
+      }
+      console.log(evt.key);
     }
-    console.log(evt.key);
-  }
+    document.addEventListener('keyup', onKeyPress);
+    return () => {
+      document.removeEventListener('keyup', onKeyPress);
+    }
+  }, [onClose]);
   return (
-    <div className={styles['modal']} onKeyUp={onKeyPress}>
+    <div className={styles['modal']}>
       <div className={`${styles['modal__header']} ml-10 mr-10`}>
         <span className={`${styles['modal__title']} text text_type_main-medium`}>{title}</span>
         <CloseIcon type={'primary'} onClick={onClose}/>
