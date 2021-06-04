41abc0f46db9c91574d16c392ddac336b84ee540
Aristov Yuriy <lonelyme.v@gmail.com>
2021-06-04 19:08:06 +0500

    Портал перенесён в Modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './modal.module.css';
+import Portal from '../portal/portal';
+import ModalOverlay from '../modal-overlay/modal-overlay';
 
 type Props = {
   title?: string;
@@ -21,12 +23,16 @@ export default function Modal({ children, title, onClose }: React.PropsWithChild
     }
   }, [onClose]);
   return (
-    <div className={styles['modal']}>
-      <div className={`${styles['modal__header']} ml-10 mr-10 mt-10`}>
-        <span className={`${styles['modal__title']} text text_type_main-medium`}>{title}</span>
-        <CloseIcon type={'primary'} onClick={onClose}/>
-      </div>
-      {children}
-    </div>
+    <Portal id={'modal'}>
+      <ModalOverlay onClick={onClose}>
+        <div className={styles['modal']}>
+          <div className={`${styles['modal__header']} ml-10 mr-10 mt-10`}>
+            <span className={`${styles['modal__title']} text text_type_main-medium`}>{title}</span>
+            <CloseIcon type={'primary'} onClick={onClose}/>
+          </div>
+          {children}
+        </div>
+      </ModalOverlay>
+    </Portal>
   )
 }
--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import Portal from '../portal/portal';
-import ModalOverlay from '../modal-overlay/modal-overlay';
 import Modal from '../modal/modal';
 import styles from './order-details.module.css';
 import orderDoneImage from '../../images/order-done.svg';
@@ -15,25 +13,21 @@ function OrderDetails({ onClose }: Props) {
     new Image().src = orderDoneImage;
   }, []);
   return (
-    <Portal id="modal-order">
-      <ModalOverlay onClick={onClose}>
-        <Modal onClose={onClose}>
-          <div className={`${styles['content']} text text_type_main-default`}>
-            <span className={`text_type_digits-large mt-2 ${styles['order-number']}`}>
-              {orderNum}
+    <Modal onClose={ onClose }>
+      <div className={ `${ styles['content'] } text text_type_main-default` }>
+            <span className={ `text_type_digits-large mt-2 ${ styles['order-number'] }` }>
+              { orderNum }
             </span>
-            <span className="mt-8">идентификатор заказа</span>
-            <span className="mt-15">
-              <img alt="check" src={orderDoneImage} width={120} height={120}/>
+        <span className="mt-8">идентификатор заказа</span>
+        <span className="mt-15">
+              <img alt="check" src={ orderDoneImage } width={ 120 } height={ 120 }/>
             </span>
-            <span className="mt-15">Ваш заказ начали готовить</span>
-            <span className="mt-2 mb-30 text_color_inactive">
+        <span className="mt-15">Ваш заказ начали готовить</span>
+        <span className="mt-2 mb-30 text_color_inactive">
               Дождитесь готвоности на орбитальной станции
             </span>
-          </div>
-        </Modal>
-      </ModalOverlay>
-    </Portal>
+      </div>
+    </Modal>
   )
 }
 
