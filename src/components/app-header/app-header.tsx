43e7e065cc26b6bd24f09192cc869a149f90bcb4
Aristov Yuriy <lonelyme.v@gmail.com>
2021-07-20 22:23:18 +0500

    Header restyling

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './app-header.module.css';
 import HeaderButton from '../header-button';
+import { Link } from 'react-router-dom';
 
 function AppHeader() {
   return (
     <header className={styles['app-header']}>
-      <Logo />
       <nav className={styles['app-header-nav']}>
         <HeaderButton text="Конструктор" type="BURGER" to="/" />
         <HeaderButton text="Лента заказов" type="LIST" to="/feed" />
       </nav>
+      <Link to="/" className={styles['app-header-logo']}>
+        <Logo />
+      </Link>
       <div className={styles['app-header-profile-wrapper']}>
         <HeaderButton text="Личный кабинет" type="PROFILE" to="/profile"/>
       </div>
@@ -18,4 +21,4 @@ function AppHeader() {
   )
 }
 
-export default AppHeader
+export default React.memo(AppHeader);
